41ea44e8615def42c79ecfcb656c0a56b1c5aa0e
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-10-04 05:39:21 -0400

    Add support for Firebase

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,42 @@
 const functions = require("firebase-functions");
 
+
+const admin = require("firebase-admin");
+
+
+// eslint-disable-next-line max-len
+const serviceAccount = require("./exploresafe-362903-firebase-adminsdk-asnln-e516bce95f.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://exploresafe-362903-default-rtdb.firebaseio.com",
+});
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const db = admin.database();
+const ref = db.ref("/");
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  ref.once("value", (data) => {
+    for (const activeRoute in data) {
+      if (activeRoute != null) {
+        console.log(activeRoute.active_route_id_0);
+      }
+    }
+    response.send(data);
+  });
+});
+
+
+/*
+exports.passiveSOSCheck = functions.pubsub.schedule("every 5 minutes")
+    .onRun((context) => {
+      console.log("This will be run every 5 minutes!");
+      return null;
+    });
+*/
