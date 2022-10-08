61a7c99032f60e7c9b030a21db180b91263663ac
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-10-08 00:55:06 -0400

    Retrieve route data

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,31 @@
 const functions = require("firebase-functions");
-
-
 const admin = require("firebase-admin");
-
-
 // eslint-disable-next-line max-len
 const serviceAccount = require("./exploresafe-362903-firebase-adminsdk-asnln-e516bce95f.json");
 
+// Connection to Realtime Database
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://exploresafe-362903-default-rtdb.firebaseio.com",
 });
-
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
 const db = admin.database();
 const ref = db.ref("/");
+
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   ref.once("value", (data) => {
-    console.log(data.active_route_id_0.user_id);
-    response.send(data.active_route_id_0.user_id);
+    for (const route in data.val()) {
+      if (route != null) {
+        const routeData = data.val()[route];
+        const passiveSOSTime = routeData.passive_sos_time;
+        const lastCallbackTime = routeData.last_callback_time;
+
+        // eslint-disable-next-line max-len
+        console.log("Passive SOS: " + passiveSOSTime + " | Last Callback Time: " + lastCallbackTime);
+      }
+    }
+    response.send(data.val());
   });
 });
 
