e7c2c2db584fc6610e8f41d95d77d5ca9e33b925
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-10-08 01:34:58 -0400

    Add SMS support

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 // eslint-disable-next-line max-len
 const serviceAccount = require("./exploresafe-362903-firebase-adminsdk-asnln-e516bce95f.json");
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
 
 // Connection to Realtime Database
 admin.initializeApp({
@@ -11,15 +14,24 @@ admin.initializeApp({
 const db = admin.database();
 const ref = db.ref("/");
 
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   ref.once("value", (data) => {
     for (const route in data.val()) {
       if (route != null) {
         const routeData = data.val()[route];
+        const routeID = routeData.route_id;
         const passiveSOSTime = routeData.passive_sos_time;
         const lastCallbackTime = routeData.last_callback_time;
+
+        const placeholder = true;
+        if (placeholder) {
+          // This acts like there is a need for an SOS notification.
+          console.log("SOS");
+          client.messages
+              .create({body: routeID, from: "+17174238730", to: "+19803584144"})
+              .then((message) => console.log(message.sid));
+        }
 
         // eslint-disable-next-line max-len
         console.log("Passive SOS: " + passiveSOSTime + " | Last Callback Time: " + lastCallbackTime);
