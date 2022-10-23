21bf3506e14fea84692c3f89b15c482b17d19ad6
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-10-22 20:29:32 -0400

    Add support for SMS messaging via Twilio

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,9 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 // eslint-disable-next-line max-len
 const serviceAccount = require("./exploresafe-362903-firebase-adminsdk-asnln-e516bce95f.json");
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountSid, authToken);
+
+const Twilio = require("twilio");
+
 
 // Connection to Realtime Database
 admin.initializeApp({
@@ -12,15 +12,20 @@ admin.initializeApp({
   databaseURL: "https://exploresafe-362903-default-rtdb.firebaseio.com",
 });
 const db = admin.database();
-const ref = db.ref("/");
+const ref = db.ref("/routes/");
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
+  // Create Twilio Client
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const twilioClient = new Twilio(accountSid, authToken);
+  console.log("Here");
   functions.logger.info("Hello logs!", {structuredData: true});
   ref.once("value", (data) => {
     for (const route in data.val()) {
       if (route != null) {
         const routeData = data.val()[route];
-        const routeID = routeData.route_id;
+        // const routeID = routeData.route_id;
         const passiveSOSTime = routeData.passive_sos_time;
         const lastCallbackTime = routeData.last_callback_time;
 
@@ -28,8 +33,17 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
         if (placeholder) {
           // This acts like there is a need for an SOS notification.
           console.log("SOS");
-          client.messages
-              .create({body: routeID, from: "+17174238730", to: "+19803584144"})
+
+
+          // Create Website with access to server
+          // Send link to SMS
+
+          twilioClient.messages
+              .create({
+                body: "Fuck yeah!",
+                to: "+19803584144", // Text this number
+                from: "+17174238730", // From a valid Twilio number
+              })
               .then((message) => console.log(message.sid));
         }
 
@@ -37,7 +51,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
         console.log("Passive SOS: " + passiveSOSTime + " | Last Callback Time: " + lastCallbackTime);
       }
     }
-    response.send(data.val());
+    response.send("Hi");
   });
 });
 
