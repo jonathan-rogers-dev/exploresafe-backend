44b9221cb0de84f90442117ca18df63e148aaea4
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-11-07 17:57:51 -0500

    Add support for automatic sos checks

--- functions/index.js
+++ functions/index.js
@@ -24,23 +24,63 @@ exports.passiveSOS = functions.https.onRequest((request, response) => {
   ref.once("value", (data) => {
     for (const route in data.val()) {
       if (route != null) {
+        let performSOS = false;
         const routeData = data.val()[route];
         // const routeID = routeData.route_id;
         // eslint-disable-next-line no-unused-vars
         const passiveSOSTime = routeData.passive_sos_time;
         // eslint-disable-next-line no-unused-vars
         const lastCallbackTime = routeData.last_callback_time;
+        const sosMessageSent = routeData.sos_message_sent;
 
-        const placeholder = false;
-        if (placeholder) {
+        const minute = 1000 * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const year = day * 365;
+
+        const currentDate = new Date(Date.now());
+        // eslint-disable-next-line max-len
+        const lastCallbackTimeDate = new Date(lastCallbackTime.year, lastCallbackTime.month, lastCallbackTime.day, lastCallbackTime.hour, lastCallbackTime.minute);
+        console.log(passiveSOSTime.value);
+        console.log((lastCallbackTimeDate));
+
+        switch (passiveSOSTime.unit) {
+          case "Years":
+            // eslint-disable-next-line max-len
+            if ((currentDate - lastCallbackTimeDate) / year > passiveSOSTime.value) {
+              performSOS = true;
+            }
+            break;
+          case "Months":
+            // eslint-disable-next-line max-len
+            if ((currentDate - lastCallbackTimeDate) / day > (28 * passiveSOSTime.value)) {
+              performSOS = true;
+            }
+            break;
+          case "Days":
+            // eslint-disable-next-line max-len
+            if ((currentDate - lastCallbackTimeDate) / day > passiveSOSTime.value) {
+              performSOS = true;
+            }
+            break;
+          case "Hours":
+            // eslint-disable-next-line max-len
+            if ((currentDate - lastCallbackTimeDate) / hour > passiveSOSTime.value) {
+              performSOS = true;
+            }
+            break;
+        }
+
+
+        if (performSOS && !sosMessageSent) {
           // This acts like there is a need for an SOS notification.
           const TWILIO_FROM_PHONE_NUMBER = process.env.TWILIO_FROM_PHONE_NUMBER;
           const TWILIO_TO_PHONE_NUMBER = process.env.TWILIO_TO_PHONE_NUMBER;
           console.log("SOS");
 
           // eslint-disable-next-line max-len
-          const message = "This is a message from ExploreSafe. At <TIME> today, an automated SOS call was made from Jonathan's ExploreSafe app to alert listed contacts that Jonathan's device has been offline for <TIME>. Please do not be frightned, however, an unexpectedly long amount of time offline may indicate that Jonathan is trapped, lost, or otherwise missing. Please use the link below to access their location history.\nhttps://exploresafe-362903.firebaseapp.com/active_route_id_0";
-
+          const message = "This is a message from ExploreSafe. At " + currentDate + "today, an automated SOS call was made from Jonathan's ExploreSafe app to alert listed contacts that Jonathan's device has been offline for <TIME>. Please do not be frightned, however, an unexpectedly long amount of time offline may indicate that Jonathan is trapped, lost, or otherwise missing. Please use the link below to access their location history.\nhttps://exploresafe-362903.firebaseapp.com/" + route;
+          console.log(message);
           // Create Website with access to server
           // Send link to SMS
 
@@ -51,6 +91,13 @@ exports.passiveSOS = functions.https.onRequest((request, response) => {
                 from: TWILIO_FROM_PHONE_NUMBER, // From a valid Twilio number
               })
               .then((message) => console.log(message.sid));
+
+
+          const updateRef = db.ref("/routes/" + route + "/");
+
+          updateRef.update({
+            "sos_message_sent": true,
+          });
         }
       }
     }
@@ -59,10 +106,92 @@ exports.passiveSOS = functions.https.onRequest((request, response) => {
 });
 
 
-/*
 exports.passiveSOSCheck = functions.pubsub.schedule("every 5 minutes")
     .onRun((context) => {
-      console.log("This will be run every 5 minutes!");
-      return null;
+      const accountSid = process.env.TWILIO_ACCOUNT_SID;
+      const authToken = process.env.TWILIO_AUTH_TOKEN;
+      const twilioClient = new Twilio(accountSid, authToken);
+      console.log("Here");
+      functions.logger.info("Hello logs!", {structuredData: true});
+      ref.once("value", (data) => {
+        for (const route in data.val()) {
+          if (route != null) {
+            let performSOS = false;
+            const routeData = data.val()[route];
+            // const routeID = routeData.route_id;
+            // eslint-disable-next-line no-unused-vars
+            const passiveSOSTime = routeData.passive_sos_time;
+            // eslint-disable-next-line no-unused-vars
+            const lastCallbackTime = routeData.last_callback_time;
+            const sosMessageSent = routeData.sos_message_sent;
+
+            const minute = 1000 * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
+            const year = day * 365;
+
+            const currentDate = new Date(Date.now());
+            // eslint-disable-next-line max-len
+            const lastCallbackTimeDate = new Date(lastCallbackTime.year, lastCallbackTime.month, lastCallbackTime.day, lastCallbackTime.hour, lastCallbackTime.minute);
+            console.log(passiveSOSTime.value);
+            console.log((lastCallbackTimeDate));
+
+            switch (passiveSOSTime.unit) {
+              case "Years":
+                // eslint-disable-next-line max-len
+                if ((currentDate - lastCallbackTimeDate) / year > passiveSOSTime.value) {
+                  performSOS = true;
+                }
+                break;
+              case "Months":
+                // eslint-disable-next-line max-len
+                if ((currentDate - lastCallbackTimeDate) / day > (28 * passiveSOSTime.value)) {
+                  performSOS = true;
+                }
+                break;
+              case "Days":
+                // eslint-disable-next-line max-len
+                if ((currentDate - lastCallbackTimeDate) / day > passiveSOSTime.value) {
+                  performSOS = true;
+                }
+                break;
+              case "Hours":
+                // eslint-disable-next-line max-len
+                if ((currentDate - lastCallbackTimeDate) / hour > passiveSOSTime.value) {
+                  performSOS = true;
+                }
+                break;
+            }
+
+
+            if (performSOS && !sosMessageSent) {
+              // This acts like there is a need for an SOS notification.
+              // eslint-disable-next-line max-len
+              const TWILIO_FROM_PHONE_NUMBER = process.env.TWILIO_FROM_PHONE_NUMBER;
+              const TWILIO_TO_PHONE_NUMBER = process.env.TWILIO_TO_PHONE_NUMBER;
+              console.log("SOS");
+
+              // eslint-disable-next-line max-len
+              const message = "This is a message from ExploreSafe. At " + currentDate + "today, an automated SOS call was made from Jonathan's ExploreSafe app to alert listed contacts that Jonathan's device has been offline for <TIME>. Please do not be frightned, however, an unexpectedly long amount of time offline may indicate that Jonathan is trapped, lost, or otherwise missing. Please use the link below to access their location history.\nhttps://exploresafe-362903.firebaseapp.com/" + route;
+              console.log(message);
+              // Create Website with access to server
+              // Send link to SMS
+
+              twilioClient.messages
+                  .create({
+                    body: message,
+                    to: TWILIO_TO_PHONE_NUMBER,
+                    from: TWILIO_FROM_PHONE_NUMBER,
+                  })
+                  .then((message) => console.log(message.sid));
+
+              const updateRef = db.ref("/routes/" + route + "/");
+
+              updateRef.update({
+                "sos_message_sent": true,
+              });
+            }
+          }
+        }
+      });
     });
-*/
